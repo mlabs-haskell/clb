cacfce5d8e8e0fd918dd94fa34a75c8e03946a93
euonymos <ilia@mlabs.city>
2024-03-16 16:21:30 -0600

    chore: haskell.nix set up

--- flake.nix
+++ flake.nix
@@ -1,47 +1,78 @@
 {
-  description = "clb";
-  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
-  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.iohkNix.url = "github:input-output-hk/iohk-nix";
-  inputs.CHaP = {
-      url = "github:input-output-hk/cardano-haskell-packages?ref=repo";
-      flake = false;
-    };
-  outputs = { self, nixpkgs, flake-utils, haskellNix, iohkNix, CHaP }:
-    let
-      supportedSystems = [
-        "x86_64-linux"	"x86_64-darwin"	"aarch64-darwin"
-      ];
-    in
-      flake-utils.lib.eachSystem supportedSystems (system:
-      let
-        overlays = [
-          # iohkNix.overlays.crypto provide libsodium-vrf, libblst and libsecp256k1.
-          iohkNix.overlays.crypto
-
-          # configure haskell.nix to use iohk-nix crypto librairies.
-          iohkNix.overlays.haskell-nix-crypto
-
-          haskellNix.overlay
-          (final: prev: {
-            hixProject =
-              final.haskell-nix.hix.project {
-                src = ./.;
-                evalSystem = system;
-                inputMap = { "https://input-output-hk.github.io/cardano-haskell-packages" = CHaP; };
-              };
-          })
-        ];
-        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
-        flake = pkgs.hixProject.flake {};
-      in flake // {
-        legacyPackages = pkgs;
-      });
+  description = "CLB Cardano Emulator";
 
   nixConfig = {
-    extra-substituters = ["https://cache.iog.io"];
-    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
+    extra-substituters = [ "https://cache.iog.io" ];
+    extra-trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
     allow-import-from-derivation = "true";
+    bash-prompt = "\\[\\e[0m\\][\\[\\e[0;2m\\]nix \\[\\e[0;1m\\]CLB \\[\\e[0;93m\\]\\w\\[\\e[0m\\]]\\[\\e[0m\\]$ \\[\\e[0m\\]";
+    cores = "1";
+    max-jobs = "auto";
+    auto-optimise-store = "true";
   };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    haskell-nix.url = "github:input-output-hk/haskell.nix";
+    iohk-nix.url = "github:input-output-hk/iohk-nix";
+    iohk-nix.inputs.nixpkgs.follows = "haskell-nix/nixpkgs";
+
+    CHaP.url = "github:input-output-hk/cardano-haskell-packages?ref=repo";
+    CHaP.flake = false;
+
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    hercules-ci-effects.url = "github:hercules-ci/hercules-ci-effects";
+  };
+
+  outputs = inputs@{ flake-parts, nixpkgs, haskell-nix, iohk-nix, CHaP, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        ./nix/pre-commit.nix
+      ];
+      debug = true;
+      systems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
+
+      perSystem = { config, system, ... }:
+        let
+          pkgs =
+            import haskell-nix.inputs.nixpkgs {
+              inherit system;
+              overlays = [
+                haskell-nix.overlay
+                iohk-nix.overlays.crypto
+                iohk-nix.overlays.haskell-nix-crypto
+              ];
+              inherit (haskell-nix) config;
+            };
+          project = pkgs.haskell-nix.cabalProject' {
+            src = ./.;
+            compiler-nix-name = "ghc928";
+            inputMap = {
+              "https://input-output-hk.github.io/cardano-haskell-packages" = CHaP;
+            };
+            shell = {
+              withHoogle = true;
+              withHaddock = true;
+              exactDeps = false;
+              # TODO: use `export CABAL_DIR=$(pwd)/.cabal`
+              shellHook = config.pre-commit.installationScript;
+              tools = {
+                cabal = { };
+                haskell-language-server = { };
+                hlint = { version = "3.6.1"; };
+                cabal-fmt = { };
+                fourmolu = { version = "0.14.0.0"; };
+                hspec-discover = { };
+              };
+            };
+          };
+          flake = project.flake { };
+        in
+        {
+          inherit (flake) checks devShells packages;
+        };
+    };
 }
---
+++ nix/pre-commit.nix
@@ -0,0 +1,32 @@
+{ inputs, ... }: {
+  imports = [
+    inputs.pre-commit-hooks.flakeModule
+  ];
+
+  perSystem = { config, ... }:
+    {
+      devShells.dev-pre-commit = config.pre-commit.devShell;
+
+      pre-commit = {
+        settings = {
+          src = ./.;
+          settings = {
+            ormolu.cabalDefaultExtensions = true;
+          };
+
+          hooks = {
+            nixpkgs-fmt.enable = true;
+            statix.enable = true;
+            deadnix.enable = true;
+
+            cabal-fmt.enable = true;
+            fourmolu = {
+              enable = true;
+              excludes = [ "\.lhs" ];
+            };
+            hlint.enable = true;
+          };
+        };
+      };
+    };
+}
